abe1107c701528f101f15d59747826f8c89cc1ca
Timothy <timothyrwashburn@gmail.com>
2024-04-20 21:18:08 -0700

    cleanup code

--- server/server.js
+++ server/server.js
@@ -6,23 +6,23 @@ const fs = require('fs');
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+	res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
 app.get('/:pageName', (req, res) => {
-  console.log(`request incoming: ${Date.now()}, ${req.path}`);
+	console.log(`request incoming: ${Date.now()}, ${req.path}`);
 
-  let pageName = req.params.pageName;
-  let pagePath = path.join(__dirname, '../client', 'pages', `${pageName}.html`);
-  if (fs.existsSync(pagePath)) {
-    res.sendFile(pagePath);
-  } else {
-    res.status(404).send('404 Not Found');
-  }
+	let pageName = req.params.pageName;
+	let pagePath = path.join(__dirname, '../client', 'pages', `${pageName}.html`);
+	if (fs.existsSync(pagePath)) {
+		res.sendFile(pagePath);
+	} else {
+		res.status(404).send('404 Not Found');
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
 
 
