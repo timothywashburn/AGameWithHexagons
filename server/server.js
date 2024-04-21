8be318da7540441c49a916d18dbcc40224cd529a
Timothy <timothyrwashburn@gmail.com>
2024-04-21 16:25:47 -0700

    switch to ejs

--- server/server.js
+++ server/server.js
@@ -4,25 +4,30 @@ const app = express();
 const fs = require('fs');
 
 const port = 3000;
+let viewsDir = __dirname + '/../client/views';
+
+app.set('view engine', 'ejs');
+app.set('views', viewsDir);
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client', 'index.html'));
+	res.render('index');
 });
 
 app.get('/:pageName', (req, res) => {
 	console.log(`request incoming: ${Date.now()}, ${req.path}`);
 
 	let pageName = req.params.pageName;
-	let pagePath = path.join(__dirname, '../client', 'pages', `${pageName}.html`);
-	if (fs.existsSync(pagePath)) {
-		res.sendFile(pagePath);
+	let pagePath = path.join(__dirname, '../client', 'views', `${pageName}.ejs`);
+
+	if (fs.existsSync(`${viewsDir}/${pageName}.ejs`)) {
+		res.render(pagePath);
 	} else {
-		res.status(404).send('404 Not Found');
+		res.status(404).render('404');
 	}
 });
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port} in ${process.env.NODE_ENV} mode`);
 });
 
 
