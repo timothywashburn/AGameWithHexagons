f0d800776c098bdff9f50d72b20b126a4eacf50b
Timothy <timothyrwashburn@gmail.com>
2024-07-13 14:37:37 -0700

    fix typescript loading with webpack

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,11 @@ module.exports = {
 	},
 	module: {
 		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: 'ts-loader'
+			}
 			// {
 			// 	test: /\.ts$/,
 			// 	exclude: /node_modules/,
