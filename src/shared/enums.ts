0c6b108ad6e4a867f093f1046a3726eff7404efd
Timothy <timothyrwashburn@gmail.com>
2024-07-13 20:42:34 -0700

    fix errors with enums

--- src/shared/enums.ts
+++ src/shared/enums.ts
@@ -1,43 +1,43 @@
-exports.NameErrorType = Object.freeze({
+export const NameErrorType = Object.freeze({
 	BAD_NAME: { id: 0x01, message: 'Illegal Name' },
 	TOO_SHORT: { id: 0x02, message: 'Name Too Short (3 Characters Minimum)' },
 	TOO_LONG: { id: 0x03, message: 'Name Too Long (30 Characters Maximum)' },
 });
 
-exports.AnnouncementType = Object.freeze({
+export const AnnouncementType = Object.freeze({
 	GAME_JOIN: { id: 0x01, message: 'Joined the game', color: 'green'},
 	GAME_LEAVE: { id: 0x02, message: 'Left the game', color: 'red'},
 });
 
-exports.RegistrationError = Object.freeze({
+export const RegistrationError = Object.freeze({
 	SUCCESS: { id: 0x00, message: 'Account created successfully' },
 	USERNAME_EXISTS: { id: 0x01, message: 'Username already exists' },
 	USERNAME_INVALID: { id: 0x02, message: 'Invalid username' },
 	PASSWORD_INVALID: { id: 0x03, message: 'Invalid password' },
 });
 
-exports.NameChangeError = Object.freeze({
+export const NameChangeError = Object.freeze({
 	SUCCESS: { id: 0x00, message: 'Name changed successfully' },
 	USERNAME_EXISTS: { id: 0x01, message: 'Username already exists' },
 	USERNAME_INVALID: { id: 0x02, message: 'Invalid username' },
 	ERROR: { id: 0x03, message: 'Error changing name' },
 });
 
-exports.EmailChangeError = Object.freeze({
+export const EmailChangeError = Object.freeze({
 	SUCCESS: { id: 0x00, message: 'Email changed successfully' },
 	EMAIL_EXISTS: { id: 0x01, message: 'Email is already linked to another account' },
 	EMAIL_INVALID: { id: 0x02, message: 'Invalid email' },
 	ERROR: { id: 0x03, message: 'Error changing email' },
 });
 
-exports.PasswordChangeError = Object.freeze({
+export const PasswordChangeError = Object.freeze({
 	SUCCESS: { id: 0x00, message: 'Password changed successfully' },
 	PASSWORD_INCORRECT: { id: 0x01, message: 'Incorrect password' },
 	INSECURE_PASSWORD: { id: 0x02, message: 'Password is insecure' },
 	ERROR: { id: 0x03, message: 'Error changing password'}
 });
 
-exports.ToastMessage = Object.freeze({
+export const ToastMessage = Object.freeze({
 	EMAIL_VERIFIED: { id: 0, message: "Your email has been successfully verified", color: "green" },
 	EMAIL_VERIFIED_ERROR: { id: 1, message: "Error verifying email. Link is likely expired.", color: "red" },
 	NAME_CHANGE_SUCCESS: { id: 2, message: "Name changed successfully", color: "green" },
@@ -49,7 +49,7 @@ exports.ToastMessage = Object.freeze({
 			"Page to add an email.", color: "orange" },
 });
 
-exports.TeamColor = Object.freeze({
+export const TeamColor = Object.freeze({
 	RED: "rgb(255, 0, 0)",
 	ORANGE: "rgb(255, 127, 0)",
 	YELLOW: "rgb(255, 255, 0)",
@@ -60,7 +60,7 @@ exports.TeamColor = Object.freeze({
 	MAGENTA: "rgb(255, 0, 255)"
 });
 
-// exports.TerrainType = Object.freeze({
+// export const TerrainType = Object.freeze({
 // 	PLAIN: {
 // 		color: new Color(134, 44, 54)
 // 	},
@@ -68,7 +68,7 @@ exports.TeamColor = Object.freeze({
 // 	MOUNTAIN: 2
 // });
 //
-// exports.ResourceType = Object.freeze({
+// export const ResourceType = Object.freeze({
 // 	NONE: 0,
 // 	DEPLETABLE: 1,
 // 	DEPLETABLE_LARGE: 2,
